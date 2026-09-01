fe88b6a444084cca3a74f9277d0ad31c64d73a96
PVYes <vs_peter@hotmail.com>
2026-09-01 14:18:00 +0200

    chirpmodel added

--- app/Models/User.php
+++ app/Models/User.php
@@ -9,23 +9,8 @@ use Illuminate\Database\Eloquent\Attributes\Hidden;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-<<<<<<< HEAD
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
-=======
-use Illuminate\Support\Carbon;
-use Illuminate\Support\Str;
-
-/**
- * @property int $id
- * @property string $name
- * @property string $email
- * @property Carbon|null $email_verified_at
- * @property string $password
- * @property string|null $remember_token
- * @property Carbon|null $created_at
- * @property Carbon|null $updated_at
- */
->>>>>>> cf34d474251447d794cbb257f62f1a9a83587e95
 #[Fillable(['name', 'email', 'password'])]
 #[Hidden(['password', 'remember_token'])]
 class User extends Authenticatable
@@ -45,19 +30,10 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
-<<<<<<< HEAD
-=======
 
-    /**
-     * Get the user's initials
-     */
-    public function initials(): string
+    public function chirps(): HasMany
     {
-        $initials = Str::initials($this->name, true);
-
-        return Str::length($initials) > 1
-            ? Str::substr($initials, 0, 1).Str::substr($initials, -1)
-            : $initials;
+        return $this->hasMany(Chirp::class);
     }
->>>>>>> cf34d474251447d794cbb257f62f1a9a83587e95
+
 }
